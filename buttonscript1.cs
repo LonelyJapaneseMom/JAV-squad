8b3c7174bd3e26fea82d9d43e1bcb29da69a3df5
GordLord22 <48309606+GordLord22@users.noreply.github.com>
2019-03-27 21:52:41 +0800

    Add files via upload
    this code is for the correct answer button.

---
+++ buttonscript1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class buttonscript1 : MonoBehaviour {
+
+	public Button button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeColor() {
+		float r = Random.Range (0f, 0f);
+		float g = Random.Range (1f, 1f);
+		float b = Random.Range (0f, 0f);
+		ColorBlock colorVar = button.colors;
+		colorVar.highlightedColor = new Color (r, g, b);
+		button.colors = colorVar;
+		scorecounter.score += 100;
+	}
+}
