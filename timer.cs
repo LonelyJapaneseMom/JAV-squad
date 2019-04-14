d4b7e1fa6e2fb8538324a3654f81af0851f3d609
GordLord22 <48309606+GordLord22@users.noreply.github.com>
2019-04-14 23:45:40 +0800

    Add files via upload

---
+++ timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour {
+
+	public int timeLeft = 10;
+	public Text countdown; 
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("LoseTime");
+		Time.timeScale = 1f; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown.text = ("Timer: " + timeLeft);
+		if (timeLeft == 0) {
+			SceneManager.LoadScene ("gameover");
+		}
+	}
+
+	public IEnumerator LoseTime() {
+		while (timeLeft > 0) {
+			yield return new WaitForSeconds (1);
+			timeLeft--;
+		} 
+	}
+}
