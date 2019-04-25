1d63df5145db5ca9466fb16386f9e1b6a02e91e5
GordLord22 <48309606+GordLord22@users.noreply.github.com>
2019-04-25 08:50:14 +0800

    Add files via upload

---
+++ mainmenubutton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenubutton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void GoMainMenu(){
+		StartCoroutine (MainMenu()); // method for ui button to load a new scene
+	}
+
+	public IEnumerator MainMenu() { // Starts a Coroutine named coroutine or any name.
+		yield return new WaitForSeconds (1); // Suspends the coroutine execution for the given amount of seconds using scaled time
+		SceneManager.LoadScene ("mainmenu"); // for the ui button to load a new scene
+	}
+}
+
