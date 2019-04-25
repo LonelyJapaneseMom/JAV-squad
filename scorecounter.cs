8237e127ae99a7569289e71f4702d60b1773a694
GordLord22 <48309606+GordLord22@users.noreply.github.com>
2019-04-25 08:44:04 +0800

    Add files via upload

--- scorecounter.cs
+++ scorecounter.cs
@@ -1,24 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class scorecounter : MonoBehaviour {
 
-	public static int score;
-	Text text; 
+	public static int score; // input for the ui text score in the inspector window
+	Text text; // declare of ui text
 
 	// Use this for initialization
 	void Awake () {
-		text = GetComponent<Text> ();
-		score = PlayerPrefs.GetInt("Score: ");
-		score = 0;
+		text = GetComponent<Text> (); // The string value this Text displays.
+		score = 0; // initialize the ui text
 	}
 
 	// Update is called once per frame
 	void Update () {
-		text.text = "Score: " + score; 
-		PlayerPrefs.SetInt ("Score: ", score);
-		hello world
+		text.text = "" + score; // The string value this Text displays.
 	}
 }
---
+++ startgame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startgame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayGame() {
+		StartCoroutine (StartGame()); // method for ui button to load a new scene
+	}
+
+	public IEnumerator StartGame() { // Starts a Coroutine named coroutine or any name.
+		yield return new WaitForSeconds (1); // Suspends the coroutine execution for the given amount of seconds using scaled time
+		SceneManager.LoadScene ("level 1"); // for the ui button to load a new scene
+	}
+}
